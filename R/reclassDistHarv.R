8d4181c24ca8921a80ff87cca11b4441b5e285ed
Craig Simpkins <simpkinscraig063@gmail.com>
2021-04-07 05:55:32 +1200

    Removed reclassHarv and renamed landscape input to template

--- R/reclassDistHarv.R
+++ R/reclassDistHarv.R
@@ -4,12 +4,12 @@
 #' @param distYr \code{sf object}. A simple feature collection covering the focal area and including the year of disturbance (preferred method) or time since disturbance as well as geometry (multipolygon) of the disturbance.
 #' @param endYr \code{numeric} default 0. Four digit date indicating the latest year to include from \code{distYr}. If no value is included function assumes \code{dateField} values indicate time since disturbance.
 #' @param numCumYrs \code{numeric}. Number of years before \code{endYr} to include.
-#' @param landCover \code{rasterLayer}. A raster of the land cover for the focal region, used as a template over which disturbance information is projected. Thus this layers dimensions determine the dimensions of the output.
+#' @param template \code{rasterLayer}. A raster of the focal region, used as a template over which disturbance information is projected. Thus this layers dimensions determine the dimensions of the output. It is recommended to use the \code{landcover} raster layer used in \code{caribouMetrics()} or \code{disturbanceMetrics()} to ensure equal dimensions
 #' @param dateField \code{character}. Name of the column in which disturbance dates/time since disturbance are recorded.
 #' 
 #' @description This function classifies disturbances/harvests that have occurred in a user defined time period. The output from this function can be used as either the \code{natDist} or \code{harv} inputs for \code{caribouMetrics()} or \code{disturbanceMetrics()}.
 #' 
-#' @return Returns a binary \code{RasterLayer} with the same dimensions as the \code{landCover} input. Values of 1 represent areas of disturbance or harvest.
+#' @return Returns a binary \code{RasterLayer} with the same dimensions as the \code{template} input. Values of 1 represent areas of disturbance or harvest.
 #' 
 #' @examples 
 #' \dontrun{
@@ -21,14 +21,14 @@
 #' reclassDist(fireYr, 
 #'             endYr = 2010, 
 #'             numCumYrs = 30, 
-#'             landCover = plcD, 
+#'             template = plcD, 
 #'             dateField = "FIRE_YEAR")
 #' }
 #' 
 #' @export
 #' 
 
-reclassDist <- function(distYr, endYr = 0, numCumYrs, landCover, dateField){
+reclassDist <- function(distYr, endYr = 0, numCumYrs, template, dateField){
   # If no endYr is defined assume years relate to time since disturbance, and 
   # thus any value smaller than numCumYrs should be recorded as a disturbance
   if(endYr == 0){
@@ -46,38 +46,7 @@ reclassDist <- function(distYr, endYr = 0, numCumYrs, landCover, dateField){
                                         endYr))
   }
   
-  out <- fasterize::fasterize(out, landCover, background = 0)
-  
-  return(out)
-}
-
-###############################################################################
-
-#' 
-#' @param harvYr \code{sf object}. A simple feature collection covering the focal area and including the year of harvest (preferred method) or time since harvest as well as geometry (multipolygon) of the harvest.
-#' @rdname reclassDist
-#' @export
-#' 
-
-reclassHarv <- function(harvYr, endYr = 0, numCumYrs, landCover, dateField){
-  # If no endYr is defined assume years relate to time since disturbance, and 
-  # thus any value smaller than numCumYrs should be recorded as a disturbance
-  if(endYr == 0){
-    out <- dplyr::filter(harvYr,
-                         dplyr::between(harvYr[[dateField]], 
-                                        numCumYrs, 
-                                        endYr))
-    
-  }else{ # If an endYr is included assume years represent dates of disturbance
-    startYr <- endYr - numCumYrs
-    
-    out <- dplyr::filter(harvYr,
-                         dplyr::between(harvYr[[dateField]], 
-                                        startYr, 
-                                        endYr))
-  }
-  
-  out <- fasterize::fasterize(out, landCover, background = 0)
+  out <- fasterize::fasterize(out, template, background = 0)
   
   return(out)
 }
