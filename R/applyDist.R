e0df0da734fe8ca33afb5031498701ed4a23a581
see24 <sarahendicott4@gmail.com>
2021-09-08 15:18:33 -0400

    speed up remove Vectorize

--- R/applyDist.R
+++ R/applyDist.R
@@ -50,6 +50,7 @@ applyDist <- function(landCover, natDist, anthroDist, tmplt){
   
   allDist16ha <- raster::stack(anthroDist, natDist) %>% 
     raster::resample(tmplt, method = "bilinear")
+  rm(anthroDist, natDist)
   
   # Get proportion of land in 16 ha area that has each type of disturbance
   watCode <- resTypeCode %>% 
@@ -60,21 +61,25 @@ applyDist <- function(landCover, natDist, anthroDist, tmplt){
   land <- 1 - landCover[[watCode]]
   
   # divide prop disturbance by prop land 
-  propLandDist <- Vectorize(function(dist, land){
+  propLandDist <- function(dist, land){
     ifelse(land < dist, 1, 
            ifelse(land == 0, 0, dist/land))
-  })
+  }
   
-  allDist16ha <- raster::overlay(allDist16ha, land, fun = propLandDist)
-  
-  
+  allDist16ha <- raster::stack(
+    raster::overlay(allDist16ha[[1]], land, fun = propLandDist),
+    raster::overlay(allDist16ha[[2]], land, fun = propLandDist)
+  )
+
   # make landCover have 0 forest classes when 16 ha area disturbed > 0.35 by natural
-  # disturbance or anthropogenic disturbance
+  # disturbance or anthropogenic disturbance  
+  anyDist35 <- max(allDist16ha > 0.35)
+  rm(allDist16ha)
   toChange <- resTypeCode %>% filter(!ResourceType %in% c("DTN", "LGW")) %>% 
     pull(code)
   
   for (i in toChange) {
-    landCover[[i]] <- raster::mask(landCover[[i]], max(allDist16ha > 0.35),
+    landCover[[i]] <- raster::mask(landCover[[i]], anyDist35,
                                    maskvalue = 1,
                                    updatevalue = 0)
   }
