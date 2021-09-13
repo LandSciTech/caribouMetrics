abbcf8c7ff8132cb9a17728321bc9de1f2a53248
see24 <sarahendicott4@gmail.com>
2021-09-13 17:04:47 -0400

    allow different high and low quantiles close #62

--- R/demographicCoefficients.R
+++ R/demographicCoefficients.R
@@ -2,7 +2,7 @@
 #'
 #' A wrapper around \code{\link{getCoefs}} to select coefficients for the
 #' appropriate model version and \code{\link{sampleCoefs}} to sample
-#' coefficients for each replicate population, for both the survival and 
+#' coefficients for each replicate population, for both the survival and
 #' recruitment models. Also optionally, generates quantiles with
 #' \code{\link{getQuantiles}}.
 #'
@@ -12,29 +12,24 @@
 #'   used in Johnson et. al. (2020)
 #' @param survivalModelNumber,recruitmentModelNumber character. Which model
 #'   number to use see \code{\link{popGrowthTableJohnsonECCC}} for options.
-#' @param useQuantiles logical. Should each replicate population be assigned
-#'   a random quantile to be used for sampling the from the distribution of
-#'   demographic parameters around the means?
+#' @param useQuantiles logical or numeric. Should each replicate population be
+#'   assigned a random quantile to be used for sampling the from the
+#'   distribution of demographic parameters around the means? If it is a numeric
+#'   vector it must be length 2 and give the low and high limits of the
+#'   quantiles to use.
 #' @param populationGrowthTable data.frame. By default
-#'   \code{\link{popGrowthTableJohnsonECCC}} is used. A custom table of model parameters
-#'   can be provided but it must match the column names of
+#'   \code{\link{popGrowthTableJohnsonECCC}} is used. A custom table of model
+#'   parameters can be provided but it must match the column names of
 #'   \code{\link{popGrowthTableJohnsonECCC}}.
 #'
-#' @return A list with elements:
-#' \describe{
-#'     \item{"modelVersion"}{The name of the model version}
-#'     \item{"coefSamples_Survival" and "coefSamples_Recruitment"}{
-#'       lists with elements:
-#'        \describe{
-#'           \item{"coefSamples"}{Bootstrapped coefficients with \code{replicates} 
-#'             rows}
-#'           \item{"coefValues"}{Coefficient values taken from 
-#'             \code{populationGrowthTable}}
-#'           \item{"quantiles"}{A vector of randomly selected quantiles between
-#'              0.025 and 0.975 with length \code{replicates}}
-#'         }
-#'       }
-#'   } 
+#' @return A list with elements: \describe{ \item{"modelVersion"}{The name of
+#'   the model version} \item{"coefSamples_Survival" and
+#'   "coefSamples_Recruitment"}{ lists with elements: \describe{
+#'   \item{"coefSamples"}{Bootstrapped coefficients with \code{replicates} rows}
+#'   \item{"coefValues"}{Coefficient values taken from
+#'   \code{populationGrowthTable}} \item{"quantiles"}{A vector of randomly
+#'   selected quantiles between 0.025 and 0.975 with length \code{replicates}} }
+#'   } }
 #'
 #' @references ECCC. 2011. Scientific assessment to inform the identification of
 #'   critical habitat for woodland caribou (Rangifer tarandus caribou), boreal
@@ -48,6 +43,7 @@
 #'   of Applied Ecology, 57(7), pp.1314-1327.
 #'   \url{https://besjournals.onlinelibrary.wiley.com/doi/full/10.1111/1365-2664.13637}
 #'
+#'
 #' @export
 
 demographicCoefficients <- function(replicates,
@@ -60,6 +56,18 @@ demographicCoefficients <- function(replicates,
   if(!all(colnames(popGrowthTableJohnsonECCC) %in% 
           colnames(populationGrowthTable))) {
     stop("populationGrowthTable must contain all colnames in popGrowthTableJohnsonECCC")
+  }
+  
+  if(length(useQuantiles) == 2){
+    if(!all(min(useQuantiles) >= 0, max(useQuantiles) <= 1)){
+      stop("useQuantiles must be between 0 and 1")
+    }
+    quantsToUse <- useQuantiles
+    useQuantiles <- TRUE
+  } else if(length(useQuantiles) == 1){
+    quantsToUse <- c(0.025, 0.975)
+  } else {
+    stop("useQuantiles must have length 1 or 2")
   }
   
   populationGrowthTable <- data.table::data.table(populationGrowthTable)
@@ -79,9 +87,13 @@ demographicCoefficients <- function(replicates,
   coefSamples_R <- sampleCoefs(DT_R, replicates)
   
   if(useQuantiles){
-  coefSamples_S$quantiles <- sample(getQuantiles(nrow(coefSamples_S$coefSamples)), 
+  coefSamples_S$quantiles <- sample(getQuantiles(nrow(coefSamples_S$coefSamples),
+                                                 low = quantsToUse[1],
+                                                 high = quantsToUse[2]), 
                                     replace = FALSE)
-  coefSamples_R$quantiles <- sample(getQuantiles(nrow(coefSamples_R$coefSamples)),
+  coefSamples_R$quantiles <- sample(getQuantiles(nrow(coefSamples_R$coefSamples),
+                                                 low = quantsToUse[1],
+                                                 high = quantsToUse[2]),
                                     replace = FALSE)
 }
   
--- R/demographicRates.R
+++ R/demographicRates.R
@@ -16,7 +16,9 @@
 #'   scenarios rows. If FALSE the returned data.frame has one row per scenario
 #'   and additional columns summarizing the variation among replicates. See
 #'   Value for details.
-#' @param useQuantiles logical. Only relevant when \code{ignorePrecision = FALSE} and
+#' @param useQuantiles logical or numeric. If it is a numeric
+#'   vector it must be length 2 and give the low and high limits of the
+#'   quantiles to use. Only relevant when \code{ignorePrecision = FALSE} and
 #'   \code{returnSample = TRUE}. If \code{useQuantiles = TRUE}, each replicate population is
 #'   assigned to a quantile of the distribution of variation around the expected
 #'   values, and remains in that quantile as covariates change. If
@@ -58,18 +60,31 @@ demographicRates <- function(covTable,
                                 useQuantiles = FALSE){
   #covTable=covTableSim; popGrowthPars = popGrowthParsSmall; ignorePrecision = FALSE; returnSample = TRUE;useQuantiles = TRUE
   
- if((length(useQuantiles)==1)&&(!useQuantiles)){
+  if(length(useQuantiles) == 2){
+    if(!all(min(useQuantiles) >= 0, max(useQuantiles) <= 1)){
+      stop("useQuantiles must be between 0 and 1")
+    }
+    quantsToUse <- useQuantiles
+    useQuantiles <- TRUE
+  } else if(length(useQuantiles) == 1){
+    quantsToUse <- c(0.025, 0.975)
+  } else {
+    stop("useQuantiles must have length 1 or 2")
+  }
+  if((length(useQuantiles)==1)&&(!useQuantiles)){
     popGrowthPars$coefSamples_S$quantiles=useQuantiles
     popGrowthPars$coefSamples_R$quantiles=useQuantiles
     
   }else{
-    q=getQuantiles(nrow(popGrowthPars$coefSamples_S$coefSamples))
+    q <- getQuantiles(nrow(popGrowthPars$coefSamples_S$coefSamples),
+                      low = quantsToUse[1],
+                      high = quantsToUse[2])
     
     if(is.null(popGrowthPars$coefSamples_S$quantiles)){
-      popGrowthPars$coefSamples_S$quantiles=sample(q,replace=F)
+      popGrowthPars$coefSamples_S$quantiles <- sample(q, replace = FALSE)
     }
     if(is.null(popGrowthPars$coefSamples_R$quantiles)){
-      popGrowthPars$coefSamples_R$quantiles=sample(q,replace=F)
+      popGrowthPars$coefSamples_R$quantiles <- sample(q, replace = FALSE)
     }
     
   }
