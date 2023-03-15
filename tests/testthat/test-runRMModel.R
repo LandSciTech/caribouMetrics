0908448b56891be33b93445ee3c3c09a4e8af56e
see24 <sarahendicott4@gmail.com>
2023-03-15 09:00:05 -0400

    update tests

--- tests/testthat/test-runRMModel.R
+++ tests/testthat/test-runRMModel.R
@@ -1,10 +1,9 @@
-library(caribouMetrics)
-library(dplyr)
-library(testthat)
 
 test_that("Runs with defaults", {
   # reduce some defaults to make fast
   # note that the default csv does not match the default startYear
   expect_warning(runRMModel(Nchains = 1, Niter = 100, Nburn = 10, Nthin = 2))
-  runRMModel(startYear = 2009, Nchains = 1, Niter = 100, Nburn = 10, Nthin = 2)
+  expect_is(runRMModel(startYear = 2009, Nchains = 1, Niter = 100, Nburn = 10,
+                       Nthin = 2),
+            "list")
 })
