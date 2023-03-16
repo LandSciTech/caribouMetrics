8fa1c276eda82445af0fc2e9134d8d5009486440
see24 <sarahendicott4@gmail.com>
2023-03-16 10:24:52 -0400

    remove ipak

--- R/CaribouDemoFns.R
+++ R/CaribouDemoFns.R
@@ -65,15 +65,6 @@ getKMSurvivalEstimates <- function(dSubset) {
   return(survData)
 }
 
-# install and load required packages
-ipak <- function(pkg) {
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) {
-    install.packages(new.pkg, dependencies = TRUE)
-  }
-  sapply(pkg, require, character.only = TRUE)
-}
-
 expandSurvivalRecord <- function(crow) {
   # crow=subset(dSubset,exit==12)[1,]
   crow <- as.numeric(crow)
