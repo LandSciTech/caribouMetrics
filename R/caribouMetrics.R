f999bb84056042fa2f8c6c2ed7c2b64b17082dd2
Josie Hughes <josie.hughes@canada.ca>
2023-10-30 16:07:10 -0400

    Refinement of documentation

--- R/caribouMetrics.R
+++ R/caribouMetrics.R
@@ -1,28 +1,27 @@
 #' caribouMetrics: Models and metrics of boreal caribou responses to forest
 #' landscapes
 #'
-#' The caribouMetrics R package provides reproducible and open source
+#' The caribouMetrics R package provides reproducible open source
 #' implementations of several models of Boreal woodland caribou (_Rangifer
-#' tarandus caribou_) demography and habitat use. These include a population and
-#' demographic model that allows users predict to demographic rates for a given
-#' level of disturbance and project population growth over time. Demographic
-#' rates are predicted using model coefficients published in [Johnson et. al.
-#' (2020)](doi:10.1111/1365-2664.13637).
-#' Population growth is projected using a two-stage demographic model with
-#' density dependence and interannual variability based on Johnson et. al.
-#' (2020) but with some modifications as described in [Dyson et al.
-#' (2022)](https://doi.org/10.1101/2022.06.01.494350). In addition to these
-#' national scale models, we provide a simple Bayesian integrated population
-#' model that integrates prior information from national analysis of
-#' demographic-disturbance relationships with available local demographic data
-#' to reduce uncertainty in population viability projections. Our model is an
-#' extension of work by [Eacker et al. (2019)](https://doi.org/10.1002/wsb.950)
-#' with some modifications and an added ability to simulate observation data
-#' given parameters that define a common caribou monitoring program.   Finally,
-#' caribouMetrics contains a set of functions which implement the caribou
-#' resource selection probability functions (RSPF) for Ontario boreal caribou
-#' ranges described in [Hornseth and Rempel
-#' (2016)](https://doi.org/10.1139/cjz-2015-0101).
+#' tarandus caribou_) demography and habitat use. A national two-stage demographic model with
+#' density dependence and interannual variability follows [Johnson et. al.
+#' (2020)](doi:10.1111/1365-2664.13637) with modifications described in [Dyson et al.
+#' (2022)](https://doi.org/10.1101/2022.06.01.494350). Demographic
+#' rates vary with disturbance as estimated by [Johnson et. al.
+#' (2020)](doi:10.1111/1365-2664.13637). The package also includes a Bayesian population model 
+#' designed to integrate prior information from Johnson et al's national analysis of demographic-disturbance 
+#' relationships with available local demographic data to reduce uncertainty in population viability projections.
+#' The Bayesian population model is an extension of work by 
+#' [Eacker et al. (2019)](https://doi.org/10.1002/wsb.950). The national model can be used to simulate 
+#' example population trajectories, and combined with a simple observation model and the Bayesian population model
+#' to show how monitoring requirements depend on landscape condition. Finally,
+#' caribouMetrics contains a set of functions which implement [Hornseth and Rempel's
+#' (2016)](https://doi.org/10.1139/cjz-2015-0101) Ontario boreal caribou resource selection model
+#' as described in  for Ontario boreal caribou
+#' ranges described in [Dyson et al. (2022)](https://doi.org/10.1101/2022.06.01.494350). Model implementation
+#' is intended to be modular and flexible, allowing reuse of components in a variety of contexts including
+#' projections of the cumulative effects of disturbance and climate change [(e.g. Stewart et al. 2023)](https://doi.org/10.1002/eap.2816)
+#' and a [Shiny app](https://landscitech.github.io/BayesianCaribouDemographicProjection/) designed to allow allow exploration of user-specified monitoring and disturbance scenarios.
 #'
 #' @docType package
 #' @name caribouMetrics
