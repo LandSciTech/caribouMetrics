da03136581fc9252eefd179882c2db8dc99feac7
Josie Hughes <josie.hughes@canada.ca>
2024-05-29 14:21:54 -0400

    Setting prior sds directly, not with multipliers.

--- R/getPriors.R
+++ R/getPriors.R
@@ -18,10 +18,11 @@
 #'   modifier is supplied here the corresponding argument below is ignored.
 #' @param returnValues logical. Default is TRUE. If FALSE returns strings for
 #'   some values showing the initial values and the modifier ie "0.9 * 1.05"
-#' @param sAnthroSlopeSEMod Multiplier for uncertainty about effect of disturbance on survival. 1 - 10
-#' @param rAnthroSlopeSEMod Multiplier for uncertainty about effect of disturbance on recruitment. 1 - 10
-#' @param sIntSEMod Multiplier for uncertainty about survival intercept. 1 - 10
-#' @param rIntSEMod Multiplier for uncertainty about recruitment intercept. 1 - 10
+#' @param sAnthroSlopeSE Standard deviation of effect of disturbance on survival.
+#' @param rAnthroSlopeSE Standard deviation of effect of disturbance on recruitment.
+#' @param rFireSlopeSE Standard deviation of effect of fire on recruitment.
+#' @param sIntSE Standard deviation of survival intercept.
+#' @param rIntSE Standard deviation of recruitment intercept.
 #' @param sSigmaMean,sSigmaSD The mean and standard deviation of the interannual 
 #' coefficient of variation for survival. 0-1. 
 #' @param rSigmaMean,rSigmaSD The mean and standard deviation of the interannual 
@@ -78,12 +79,13 @@
 getPriors <- function(modList = NULL,
                       survivalModelNumber = "M1",
                       recruitmentModelNumber = "M4",
-                      rAnthroSlopeSEMod = 4,
-                      sAnthroSlopeSEMod = 3,
-                      sIntSEMod = 5,
+                      rAnthroSlopeSE = 0.0061,
+                      rFireSlopeSE = 0.002,
+                      sAnthroSlopeSE = 0.0004,
+                      sIntSE = 0.06,
                       sSigmaMean = 0.08696 * 0.4,
                       sSigmaSD = 0.03,
-                      rIntSEMod = 3,
+                      rIntSE = 0.18,
                       rSigmaMean = 0.46 * 0.5,
                       rSigmaSD = 0.22,
                       qMin=0, qMax =0.6, 
@@ -154,17 +156,17 @@ getPriors <- function(modList = NULL,
   if (returnValues) {
     betaPriors <- list(
       l.R.Prior1 = rPriorCoefs$Intercept,
-      l.R.Prior2 = rPriorStdErrs$Intercept * modList$rIntSEMod,
+      l.R.Prior2 = modList$rIntSE,
       beta.Rec.anthro.Prior1 = rPriorCoefs$Anthro,
-      beta.Rec.anthro.Prior2 = rPriorStdErrs$Anthro * modList$rAnthroSlopeSEMod,
+      beta.Rec.anthro.Prior2 = modList$rAnthroSlopeSE,
       beta.Rec.fire.Prior1 = rPriorCoefs$fire_excl_anthro,
-      beta.Rec.fire.Prior2 = rPriorStdErrs$fire_excl_anthro,
+      beta.Rec.fire.Prior2 = modList$rFireSlopeSE,
       sig.R.Prior1 = modList$rSigmaMean,
       sig.R.Prior2 = modList$rSigmaSD,
       l.Saf.Prior1 = sPriorCoefs$Intercept,
-      l.Saf.Prior2 = sPriorStdErrs$Intercept * modList$sIntSEMod,
+      l.Saf.Prior2 = modList$sIntSE,
       beta.Saf.Prior1 = sPriorCoefs$Anthro,
-      beta.Saf.Prior2 = sPriorStdErrs$Anthro * modList$sAnthroSlopeSEMod,
+      beta.Saf.Prior2 = modList$sAnthroSlopeSE,
       sig.Saf.Prior1 = modList$sSigmaMean,
       sig.Saf.Prior2 = modList$sSigmaSD,
       bias.Prior1 = bias.Prior1,
@@ -182,17 +184,17 @@ getPriors <- function(modList = NULL,
   } else {
     betaPriors <- list(
       l.R.Prior1 = rPriorCoefs$Intercept,
-      l.R.Prior2 = rount(rPriorStdErrs$Intercept*modList$rIntSEMod, 4),
+      l.R.Prior2 = round(modList$rIntSE, 4),
       beta.Rec.anthro.Prior1 = rPriorCoefs$Anthro,
-      beta.Rec.anthro.Prior2 = round(rPriorStdErrs$Anthro*modList$rAnthroSlopeSEMod, 4),
+      beta.Rec.anthro.Prior2 = round(modList$rAnthroSlopeSE, 4),
       beta.Rec.fire.Prior1 = rPriorCoefs$fire_excl_anthro,
-      beta.Rec.fire.Prior2 = round(rPriorStdErrs$fire_excl_anthro,4),
+      beta.Rec.fire.Prior2 = round(modList$rFireSlopeSE,4),
       sig.R.Prior1 = modList$rSigmaMean,
       sig.R.Prior2 = modList$rSigmaSD,
       l.Saf.Prior1 = sPriorCoefs$Intercept,
-      l.Saf.Prior2 = round(sPriorStdErrs$Intercept*modList$sIntSEMod, 4),
+      l.Saf.Prior2 = round(modList$sIntSE, 4),
       beta.Saf.Prior1 = sPriorCoefs$Anthro,
-      beta.Saf.Prior2 = round(sPriorStdErrs$Anthro*modList$sAnthroSlopeSEMod, 4),
+      beta.Saf.Prior2 = round(modList$sAnthroSlopeSE, 4),
       sig.Saf.Prior1 = modList$sSigmaMean,
       sig.Saf.Prior2 = modList$sSigmaSD,
       bias.Prior1 = bias.Prior1,
