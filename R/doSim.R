2374cb71875bd465f08be4853e85e0ae044ecc91
Hughes <Josie.Hughes@ec.gc.ca>
2025-07-25 16:15:29 -0400

    Moved doSim helper function from CaribouDemographyBasicApp.
    In progress: vignette.

--- R/doSim.R
+++ R/doSim.R
@@ -55,3 +55,28 @@ doSim <- function(numSteps, numPops, N0, R_bar, S_bar, R_sd, S_sd, R_iv_cv, S_iv
 
   return(bind_rows(mod_mean, mod_samps))
 }
+
+caribouPopSim <- function(N0, numSteps, R_samp, S_samp, interannualVar, ...) {
+  for (ts in 1:numSteps) {
+    if (ts == 1) {
+      out <- caribouPopGrowth(rep(N0, length(R_samp)),
+                              numSteps = 1,
+                              interannualVar = interannualVar,
+                              R_bar = R_samp, S_bar = S_samp, ...
+      )
+      out$id <- seq(1:nrow(out))
+      out$time <- ts
+      outBit <- out
+    } else {
+      outBit <- caribouPopGrowth(outBit$N,
+                                 numSteps = 1, interannualVar = interannualVar,
+                                 R_bar = R_samp, S_bar = S_samp, ...
+      )
+      outBit$id <- seq(1, nrow(outBit))
+      outBit$time <- ts
+      out <- rbind(out, outBit)
+    }
+  }
+  return(out)
+}
+
