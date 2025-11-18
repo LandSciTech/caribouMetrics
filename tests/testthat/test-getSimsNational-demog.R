9fd80efc2f645f2a3e228da5b6251717ae169bcd
Sarah Endicott <sarah.endicott@ec.gc.ca>
2025-11-18 09:02:44 -0500

    remove old tests accidentally added back

--- tests/testthat/test-getSimsNational-demog.R
+++
@@ -1,6 +0,0 @@
-test_that("cacheing happens", {
-  # clear the cache
-  rm(list = ls(envir = cacheEnv), envir = cacheEnv)
-  expect_message(expect_warning(getSimsNational()), "Updating cached")
-  expect_message(getSimsNational(), "saved object")
-})
