2877ebab5e43ddb62dff07a1180a5029e7e49891
Hughes <Josie.Hughes@ec.gc.ca>
2026-09-10 11:31:42 -0400

    Enabling https://github.com/LandSciTech/CaribouDemographyMisc/blob/main/docs/YSWorkflow.Rmd

--- R/trajectoriesFromSummary.R
+++ R/trajectoriesFromSummary.R
@@ -25,13 +25,15 @@ trajectoriesFromSummary <- function(replicates, N0, Rbar, Sbar, Riv, Siv,
   # TO DO:
   # See https://github.com/LandSciTech/caribouMetrics/issues/146
   testTable(Rbar, req_col_names = c("PopulationName", "Annual", "Year", "mean", "sd"))
-  testTable(Riv, req_col_names = c("R_iv_mean", "R_iv_shape"))
   testTable(Sbar, req_col_names = c("PopulationName", "Annual", "Year", "mean", "sd"))
-  testTable(Siv, req_col_names = c("S_iv_mean", "S_iv_shape"))
   
   if(type=="beta"){
+    testTable(as.data.frame(Riv), req_col_names = c("R_cv_min", "R_cv_max"))
+    testTable(as.data.frame(Siv), req_col_names = c("S_cv_min", "S_cv_max"))
     results <- ratesFromBetaSummary(Rbar, Sbar, Riv, Siv, replicates, nthin,varPersists)
   }else{
+    testTable(as.data.frame(Riv), req_col_names = c("R_iv_mean", "R_iv_shape"))
+    testTable(as.data.frame(Siv), req_col_names = c("S_iv_mean", "S_iv_shape"))
     results <- ratesFromLogisticSummary(Rbar, Sbar, Riv, Siv, replicates, nthin,varPersists)
   }
   rr <- trajectoriesFromBayesian(results, N0 = N0,returnSamples = returnSamples, 
